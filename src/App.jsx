99101d4ff897d59df9cc843d8894c734c99c3129
mase1509-design <mase1509@googlemail.com>
2025-11-03 14:45:07 +0100

    Update App.jsx
    Fix of wrong code

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,6 @@ import ExerciseList from "./components/ExerciseList";
 import ExerciseForm from "./components/ExerciseForm";
 import "./styles.css";
 
-<header style={{ textAlign: "center", marginTop: "20px" }}>
-  <img
-    src={`${import.meta.env.BASE_URL || ''}logo.png`}
-    alt="Trainingsguru Logo"
-    style={{ height: "100px" }}
-  />
-</header>
-
 function App() {
   const [exercises, setExercises] = useState([]);
 
@@ -29,7 +21,12 @@ function App() {
 
   return (
     <div>
-      <header>
+      <header style={{ textAlign: "center", marginTop: "20px" }}>
+        <img
+          src={`${import.meta.env.BASE_URL || ''}logo.png`}
+          alt="Trainingsguru Logo"
+          style={{ height: "100px" }}
+        />
         <h1>Fitness Tracker</h1>
       </header>
       <div className="container">
@@ -41,4 +38,3 @@ function App() {
 }
 
 export default App;
-
